f90f5018f5d026a271e8d33d0b21ac66d85e8dd9
Alan Jordan <alanpjordan@gmail.com>
2017-12-28 11:46:24 +1300

    DB tests now working

--- server/lib/__tests__/users.test.js
+++ server/lib/__tests__/users.test.js
@@ -2,19 +2,32 @@ import knex from "knex";
 import { findUserByEmail } from "../users";
 const config = require("../../../knexfile").test;
 
-beforeEach(() => {
-  const connection = knex(config);
-  connection.migrate.latest().then(() => {
-    connection.seed.run();
+let connection = knex(config);
+
+beforeAll(() => {
+  return connection.migrate.latest().then(() => {
+    return connection.seed.run();
   });
 });
 
-afterEach(() => {
-  connection.destroy();
+afterAll(() => {
+  return connection.destroy();
 });
 
 describe("testing findUserByEmail", () => {
   it("should return the db entry for alanpjordan@gmail.com when that is passed", () => {
-    expect(findUserByEmail(connection, "alanpjordan@gmail.com"));
+    return findUserByEmail(connection, "alanpjordan@gmail.com").then(data => {
+      expect(data[0].id).toBe(1);
+      expect(data[0].user_first_name).toBe("Alan");
+      expect(data[0].user_surname).toBe("Jordan");
+    });
+  });
+  it("should error return an emptry array when a user is not found", () => {
+    return findUserByEmail(connection, "alordan@gmail.com").then(data => {
+      expect(data[0]).toBeFalsy();
+    });
+  });
+  it("should throw an error when no connection is available", () => {
+    expect(() => findUserByEmail(noconnection, "test")).toThrowError();
   });
 });
--- server/lib/users.js
+++ server/lib/users.js
@@ -1,2 +1,7 @@
-export const findUserByEmail = (connection, email) =>
-  connection("users").where("email", email);
+export const findUserByEmail = (connection, email) => {
+  return connection("users")
+    .where("user_email", email)
+    .on("query-error", (err, obj) => {
+      return new Error(`Error: ${err}`);
+    });
+};
